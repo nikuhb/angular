66670beefb7783fb46bfc7c22f788d2fc61bcfcb
niku <nik_116@yahoo.com>
2020-09-05 22:18:34 +0430

    routing2

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeCarouselComponent } from './components/home-carousel/home-carousel.component';
-
+import { HomeRoutingModule } from './routes/home-routing.module';
+import { from } from 'rxjs';
 
 
 @NgModule({
   declarations: [HomeComponent, HomeCarouselComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule,
   ]
 })
 export class HomeModule { }
