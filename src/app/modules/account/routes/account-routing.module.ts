0f7c94064b2902c2d89dce3476d3af12ee81f548
niku <nik_116@yahoo.com>
2020-09-13 00:09:48 +0430

    account routing module is set

--- src/app/modules/account/routes/account-routing.module.ts
+++ src/app/modules/account/routes/account-routing.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from '../components/account/account.component';
 import { AccountOrderComponent } from '../components/account-order/account-order.component';
+import { AccountPersonalInformationComponent } from '../components/account-personal-information/account-personal-information.component';
+import { AccountVoucherComponent } from '../components/account-voucher/account-voucher.component';
+import { AccountWishlistComponent } from '../components/account-wishlist/account-wishlist.component';
+
 const routes: Routes = [
+  {
+    path: 'order',
+    component: AccountOrderComponent,
+  },
   {
     path: '',
     component: AccountComponent,
   },
   {
-    path: 'Amir',
-    component: AccountOrderComponent,
-  }
+    path: 'personal-information',
+    component: AccountPersonalInformationComponent,
+  },
+  {
+    path: 'voucher',
+    component: AccountVoucherComponent,
+  },
+  {
+    path: 'wishlist',
+    component: AccountWishlistComponent,
+  },
 ];
 
 @NgModule({
